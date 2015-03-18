2bc4f5eac38e50ab14928d4a288b4fbec296a6fe
Cyber Grand Challenge Framework Team <CyberGrandChallenge@darpa.mil>
2015-03-18 18:19:05 -0400

    cqe rc4

---
+++ privileges.h
@@ -0,0 +1,31 @@
+/*
+* Copyright (C) 2014 - Brian Caswell <bmc@lungetech.com>
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+#ifndef PRIVILEGES_H
+#define PRIVILEGES_H
+
+void setup_chroot(const char *directory);
+void drop_privileges(const uid_t uid, const gid_t gid);
+uid_t get_unused_uid(void);
+gid_t get_unused_gid(void);
+
+#endif
